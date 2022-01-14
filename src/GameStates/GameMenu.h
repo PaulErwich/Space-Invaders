5893f168336e014d37c742f0437849b580a7605c
Paul Erwich <perwich@outlook.com>
2022-01-14 17:50:37 +0000

    Started adding GameState Machine

---
+++ src/GameStates/GameMenu.h
@@ -0,0 +1,12 @@
+//
+// Created by MLG Erwich on 14/01/2022.
+//
+
+#ifndef SPACE_INVADERS_GAMEMENU_H
+#define SPACE_INVADERS_GAMEMENU_H
+
+class GameMenu
+{
+};
+
+#endif // SPACE_INVADERS_GAMEMENU_H
---
+++ src/GameStates/GameStateBase.h
@@ -0,0 +1,16 @@
+//
+// Created by MLG Erwich on 14/01/2022.
+//
+
+#ifndef SPACE_INVADERS_GAMESTATEBASE_H
+#define SPACE_INVADERS_GAMESTATEBASE_H
+
+
+
+class GameStateBase {
+
+};
+
+
+
+#endif //SPACE_INVADERS_GAMESTATEBASE_H
